b4a734eac86cbed667adf85d6895cbc2db795186
Jupp Müller <jupp0r@gmail.com>
2011-12-23 02:05:31 +0100

    First Ruby exercise, really boring one, too

---
+++ 1_ruby/1_misc.rb
@@ -0,0 +1,12 @@
+# first exercise
+puts "Hello, World."
+
+# second exercise
+puts "Hello, Ruby.".index "Ruby"
+
+# third exercise
+10.times { puts "Jupp" }
+
+#fourth exercise
+(1..10).each { |i| puts "This is sentence #{i}" }
+ 
