066e318d4decd3a1b2f9a3dc9bf1c5e7a1153bf3
Jupp Müller <jupp0r@gmail.com>
2011-12-23 09:50:39 +0100

    Ruby exercise 4: grep (even enhanced it with color)

---
+++ 1_ruby/4_grep.rb
@@ -0,0 +1,20 @@
+RED = "\033[31m"
+RESET = "\033[0m"
+
+pattern = ARGV.shift
+exit "You must specify a pattern" unless pattern
+
+filename = ARGV.shift
+exit "You must specify a file name!" unless filename
+
+grep_file = File.new filename
+
+cnt = 1
+
+while line = grep_file.gets
+  if line.match pattern
+    new_line = line.sub pattern, "#{RED}#{pattern}#{RESET}"
+    puts "#{cnt}: #{new_line}"
+  end
+  cnt += 1
+end
